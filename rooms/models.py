3ccfcd7ad33055d9a1d4945e120799f73f5b4885
slimebathtub <slimebathtub@gmail.com>
2025-04-22 00:49:53 -0700

    feat: Implement Room model, create initial migration, and add room management templates

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from resources.models import ResourceEntity
+
 # Create your models here.
+class Room(ResourceEntity):
+    people_available = models.IntegerField()
+
+    
