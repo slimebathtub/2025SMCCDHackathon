f686921ba7f6b5639faf9b7661b79a5da6df9295
slimebathtub <slimebathtub@gmail.com>
2025-04-22 21:06:17 -0700

    feat: Enhance dashboard with sidebar styling and dynamic content rendering

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from resources.models import Item
+from core.models import Center
+from tutoring.models import TutoringDailySchedule
+
 def dashboard_view(request):
-    return render(request, 'dashboard/dashboard.html')
+    items = Item.objects.all()
+    tutor_schedules = TutoringDailySchedule.objects.all()
+    return render(request, 'dashboard/dashboard.html', {
+        'items': items,
+        'tutor_session': tutor_schedules,
+    })
