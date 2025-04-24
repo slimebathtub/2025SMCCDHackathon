8b9b9328b93a92570e574bd1d2fa18c35e45cdf7
FOMKA555 <47538458+FOMKA555@users.noreply.github.com>
2025-04-23 20:09:52 -0700

    made views.py for accounts, log in request

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login_page(request):
+    if request.method == 'POST':
+        userID = request.POST.get('ID')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(userID = userID).exists():
+            messages.error(request, 'Invalid Resource ID')
+            return redirect('/login/')
+
+        user = authenticate(username=userID, password=password)
+
+        if user is None:
+            messages.error(request, 'Invalid Password')
+            return redirect('/login/')
+        else:
+            login(request, user)
+            return redirect('/#/')
+
+    return render(request, 'login.html')
