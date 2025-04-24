dad697ef6bf0c265521df3b86aa2bae50fb65428
FOMKA555 <47538458+FOMKA555@users.noreply.github.com>
2025-04-23 20:08:56 -0700

    managed urls.py for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
+from .views import login_page, home
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    
+    path('home/', home, name='home'),
+    path('admin/', admin.site.urls),
+    path ('login/', login_page, name='login_page'),
 ]
+
+
+
+
+urlpatterns += staticfiles_urlpatterns()
