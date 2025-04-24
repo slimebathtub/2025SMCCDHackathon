b3d757e234ec70b614a64c744aeb42c7ffb25a79
slimebathtub <slimebathtub@gmail.com>
2025-04-24 04:06:36 -0700

    feat: Update URL patterns and adjust navbar height for improved layout

--- resources/urls.py
+++ resources/urls.py
@@ -3,6 +3,6 @@ from .views import sourcepage_view
 from resources import views
 
 urlpatterns = [
-    path('resources/', sourcepage_view, name='resources-list'),
+    path('', sourcepage_view, name='resources-list'),
     # path('items/', views.center_item_list, name='center_item_list'),
 ]
