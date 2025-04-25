45a40cf4b84bd5ccd09de341f6acf1e80c98abf6
slimebathtub <slimebathtub@gmail.com>
2025-04-25 06:07:20 -0700

    feat: Update database settings to use environment variables for PostgreSQL

--- campus_site/settings.py
+++ campus_site/settings.py
@@ -93,9 +93,16 @@ WSGI_APPLICATION = "campus_site.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
         'default': dj_database_url.config(conn_max_age=600)
     }
 }
+
 
 
 # Password validation
