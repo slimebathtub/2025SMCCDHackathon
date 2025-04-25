bb8d80700271b874051b2edcede24864781a2fd3
slimebathtub <slimebathtub@gmail.com>
2025-04-24 19:22:04 -0700

    Refactor code structure for improved readability and maintainability

--- campus_site/urls.py
+++ campus_site/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('homepage/', include('core.urls')),
+    path('', include('core.urls')),
     path('resources/', include('resources.urls')),
     path('tutoring/', include('tutoring.urls')),
     path('accounts/', include('accounts.urls')),
