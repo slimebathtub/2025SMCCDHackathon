a8128cb108afaf157f64b628c8558b7aa1b890d1
FOMKA555 <47538458+FOMKA555@users.noreply.github.com>
2025-04-24 18:43:09 -0700

    refactor: update URL patterns to import views correctly and remove unused home path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from .views import login_page, home
+from . import views
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('home/', home, name='home'),
     path('admin/', admin.site.urls),
-    path ('login/', login_page, name='login_page'),
+    path ('login/', views.login_page, name='login'),
+
+
 ]
 
 
